5549e47fe5708e7b9425f5a8301e94fdefa2c97b
webabbey247 <balogun.abiodunlive@gmail.com>
2026-09-09 11:22:51 -0500

    fix: reolved single program page

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -388,8 +388,7 @@ const ProgramsPage = () => {
 
                   return (
                     <Link
-                      // href={`/programs/${prog.slug}`}
-                      href="#"
+                      href={`/programs/${prog.slug}`}
                       key={prog.id}
                       className="group bg-white rounded-sm overflow-hidden transition-all duration-300 cursor-pointer hover:-translate-y-1 hover:shadow-[0_16px_40px_rgba(13,27,42,0.1)]"
                     >
